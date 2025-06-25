57a7db0f113f4349f3413684e97f16aa5bbeae98
Travis Reed <tdreed@gmail.com>
2025-06-25 16:52:28 -0700

    Refactor Pete binary: move functionality to main.rs and remove standalone file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use neo4rs::Graph;
+use psyche_rs::pete;
 use psyche_rs::{MemoryStore, Neo4jStore, Will};
 use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
     let uri = std::env::var("NEO4J_URI").unwrap_or_else(|_| "127.0.0.1:7687".into());
     let user = std::env::var("NEO4J_USER").unwrap_or_else(|_| "neo4j".into());
     let pass = std::env::var("NEO4J_PASS").unwrap_or_else(|_| "neo4j".into());
@@ -13,6 +16,5 @@ async fn main() -> anyhow::Result<()> {
         .map_err(|e| anyhow::anyhow!(format!("{:?}", e)))?;
     let store: Arc<dyn MemoryStore> = Arc::new(Neo4jStore { client: graph });
     let _will = Will::new(store);
-    // Application logic would go here
-    Ok(())
+    pete::launch_default_pete().await
 }
