f694e729e4c6cd8fd0dd54c44dd7d6260fcd5b16
Travis Reed <dancxjo@users.noreply.github.com>
2025-06-24 23:29:27 -0700

    Add mouth trait and memory query support

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::sync::Arc;
-use psyche_rs::{Neo4jMemoryStore, MemoryStore, Will};
 use neo4rs::Graph;
+use psyche_rs::{MemoryStore, Neo4jMemoryStore, Will};
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -8,7 +8,11 @@ async fn main() -> anyhow::Result<()> {
     let user = std::env::var("NEO4J_USER").unwrap_or_else(|_| "neo4j".into());
     let pass = std::env::var("NEO4J_PASS").unwrap_or_else(|_| "neo4j".into());
 
-    let graph = Arc::new(Graph::new(&uri, &user, &pass).await.map_err(|e| anyhow::anyhow!(format!("{:?}", e)))?);
+    let graph = Arc::new(
+        Graph::new(&uri, &user, &pass)
+            .await
+            .map_err(|e| anyhow::anyhow!(format!("{:?}", e)))?,
+    );
     let store: Arc<dyn MemoryStore> = Arc::new(Neo4jMemoryStore { graph });
     let _will = Will::new(store);
     // Application logic would go here
