86fd968ed6021d3cdbd414bfff85e0f49b0e982f
Travis Reed <dancxjo@users.noreply.github.com>
2025-06-28 21:00:13 -0700

    Group module re-exports

---
+++ daringsby/src/motors.rs
@@ -0,0 +1,9 @@
+/// Motor implementations used by the Daringsby binary.
+///
+/// # Examples
+/// ```
+/// use daringsby::motors::LoggingMotor;
+/// ```
+pub use crate::logging_motor::LoggingMotor;
+pub use crate::look_motor::LookMotor;
+pub use crate::mouth::Mouth;
---
+++ daringsby/src/streams.rs
@@ -0,0 +1,8 @@
+/// Stream sources provided by the Daringsby runtime.
+///
+/// # Examples
+/// ```
+/// use daringsby::streams::SpeechStream;
+/// ```
+pub use crate::look_stream::LookStream;
+pub use crate::speech_stream::SpeechStream;
