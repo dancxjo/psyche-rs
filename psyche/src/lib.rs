96d7c3d22293a95912c70e5f486c960e031cd0bd
Travis Reed <dancxjo@users.noreply.github.com>
2025-07-16 15:46:56 -0700

    Get rid of sample code

--- psyche/src/lib.rs
+++ psyche/src/lib.rs
@@ -7,12 +7,3 @@ pub mod llm;
 pub mod memory;
 pub mod models;
 
-/// Example helper that adds two numbers.
-///
-/// ```
-/// use psyche::add;
-/// assert_eq!(add(2, 2), 4);
-/// ```
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
