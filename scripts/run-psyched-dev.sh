d7261f61de0903e6d9d6c0446e09cad2e681bb14
Travis Reed <tdreed@gmail.com>
2025-07-17 15:35:09 -0700

    This should not be implemented here.

--- scripts/run-psyched-dev.sh
+++ scripts/run-psyched-dev.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+if [ -z "$1" ]; then
+    set -- "layka"
+fi
+
 # Run the psyche daemon in development mode
-cargo run -p psyched -- --socket ./layka.sock --log-level debug
+cargo run -p psyched -- --soul all_souls/$1 --socket ./$1.sock --log-level debug
