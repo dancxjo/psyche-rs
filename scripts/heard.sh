d1a741f847cc4109b6b11f63d8648632267cb29a
Travis Reed <tdreed@gmail.com>
2025-07-17 22:09:06 -0700

    feat(heard): add log level option to heard.sh script

--- scripts/heard.sh
+++ scripts/heard.sh
@@ -4,4 +4,4 @@ if [ -z "$1" ]; then
     set -- "layka"
 fi
 
-WHISPER_MODEL=whisper-base.en.bin cargo run -p heard -- --socket ./quick.sock --listen ./ear.sock
+WHISPER_MODEL=whisper-base.en.bin cargo run -p heard -- --socket ./quick.sock --listen ./ear.sock --log-level debug
